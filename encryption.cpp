28d4b13fbf5c0b5c6d7e6802c798e9686e726367
Olha <olhalashchukevych@gmail.com>
2024-02-27 03:50:59 -0600

    Add bidirectional decryption feature
    Introduced an option to decrypt messages in both directions

--- encryption.cpp
+++ encryption.cpp
@@ -3,6 +3,7 @@
 #include <cctype>
 #include <algorithm>
 
+// Stores all related to cryptography data
 struct EncryptionParameters {
     std::string userMessage;
     int userKey;
@@ -15,6 +16,7 @@ void displayMenu();
 void getUserMessage(EncryptionParameters &encryption);
 void encryptMessage(EncryptionParameters &encryption, const char *letters);
 void decryptMessage(EncryptionParameters &encryption, const char *letters);
+void convertToUpper(std::string &message);
 
 int main() {
     const char letters[] = {'A', 'B', 'C', 'D','E', 'F', 'G', 'H','I', 'J', 'K', 'L','M', 'N', 'O', 'P','Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '};
@@ -30,7 +32,8 @@ int main() {
             break;
         case 2:
             getUserMessage(encryption);
-            decryptMessage(encryption, letters); // FIXME: Finish decryptMessage()
+            decryptMessage(encryption, letters);
+            std::cout << "Decrypted message: " << encryption.userMessage << std::endl;
             break;
         case 3:
             break;
@@ -41,12 +44,15 @@ int main() {
     return 0;
 }
 
+// Displays main menu with options
 void displayMenu() {
     std::cout << "1. Encrypt  message" << std::endl;
     std::cout << "2. Decrypt message" << std::endl;
     std::cout << "3. Show decrypted possibilities" << std::endl;
     std::cout << "4. Break code" << std::endl;
 }
+
+// Prompts user for an option between encryption, decryption, showing all possibilities, and breaking code
 int getUserChoice() {
     int userChoice;
 
@@ -57,6 +63,8 @@ int getUserChoice() {
 
     return userChoice;
 }
+
+// Prompts user for a shift key, forward or backward direction of encryption/decryption, and a message to encrypt/decrypt
 void getUserMessage(EncryptionParameters &encryption) {
     std::string tempVal;
 
@@ -68,12 +76,20 @@ void getUserMessage(EncryptionParameters &encryption) {
     getline(std::cin, tempVal);
     getline(std::cin, encryption.userMessage);
 }
+
+// Converts message to all uppercase letters
+void convertToUpper(std::string &message) {
+    for (int i = 0, len = message.size(); i < len; i++) {
+        message[i] = toupper(message[i]);
+    }
+}
+
+// Encrypts message in forward or backward 
 void encryptMessage(EncryptionParameters &encryption, const char *letters) {
     int shiftValue = encryption.userKey % 27;
+    convertToUpper(encryption.userMessage);
 
     for (int i = 0, len = encryption.userMessage.size(); i < len; i++) {
-        encryption.userMessage[i] = toupper(encryption.userMessage[i]);
-
         if (isalpha(encryption.userMessage[i]) || isspace(encryption.userMessage[i])) {
             char tmpChar = (isspace(encryption.userMessage[i])) ? (91 - 'A') : encryption.userMessage[i] - 'A';
             int letterIndex = (tmpChar + shiftValue) % 27;
@@ -87,7 +103,21 @@ void encryptMessage(EncryptionParameters &encryption, const char *letters) {
     }
 }
 
+// Decrypts message in forward or backward 
 void decryptMessage(EncryptionParameters &encryption, const char *letters) {
-    std::cout << "FIXME: Finish decryptMessage()" << std::endl;
-    return;
+    int shiftValue = encryption.userKey % 27;
+    convertToUpper(encryption.userMessage);
+    if (encryption.userDirection == 'b') {
+        reverse(encryption.userMessage.begin(), encryption.userMessage.end());
+    }
+
+    for (int i = 0, len = encryption.userMessage.size(); i < len; i++) {
+        if (isalpha(encryption.userMessage[i]) || isspace(encryption.userMessage[i])) {
+            char tmpChar = (isspace(encryption.userMessage[i])) ? (91 - 'A') : encryption.userMessage[i] - 'A';
+            int letterIndex = (tmpChar - shiftValue) % 27;
+            letterIndex = (letterIndex < 0) ? (27 + letterIndex) : letterIndex;
+            encryption.userMessage[i] = letters[letterIndex];
+            shiftValue = (shiftValue + letterIndex + encryption.userKey) % 27;
+        }
+    }
 }
